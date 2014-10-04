1c1a285bec6f7c0dc7344bc902f91fe817fd1bb1
Ezodev <ezo.dev@gmail.com>
2014-10-04 22:47:21 +0200

    Moved camera control to separated task

--- src/tasks/Controller.cpp
+++ src/tasks/Controller.cpp
@@ -3,13 +3,8 @@
 #include "SFMLInputProxy.h"
 #include "InputEcho.h"
 #include "VerletIntegrator.h"
+#include "CameraMouseController.h"
 #include "events/system/ApplicationClosed.h"
-#include "events/system/MouseWheelMoved.h"
-#include "events/system/MouseButtonPressed.h"
-#include "events/system/MouseButtonReleased.h"
-#include "events/system/MouseMoved.h"
-#include "components/PositionComponent.h"
-#include "components/MovementComponent.h"
 #include "components/RenderingComponent.h"
 
 void Controller::update() {
@@ -21,48 +16,15 @@ void Controller::receive(ApplicationClosedEvent& closeRequest) {
 	engine.stop();
 }
 
-void Controller::receive(MouseWheelMoved& mouseWheelEvent) {
-	sf::View currentView = window.getView();
-	currentView.zoom(1.f + zoomFactor * mouseWheelEvent.ticks);
-	window.setView(currentView);
-}
-
-void Controller::receive(MouseButtonPressed& mousePress) {
-	if(mousePress.button.button == panViewMouseButtonCode) {
-		mouseMode = PanView;
-	}
-}
-
-void Controller::receive(MouseButtonReleased& mouseRelease) {
-	if(mouseRelease.button.button == panViewMouseButtonCode) {
-		mouseMode = Nothing;
-	}
-}
-
-void Controller::receive(MouseMoved& mouseMoved) {
-	if(mouseMode == PanView) {
-		sf::View currentView = window.getView();
-		currentView.move(-mouseMoved.worldDelta.x * panViewXFactor, -mouseMoved.worldDelta.y * panViewYFactor);
-		window.setView(currentView);
-	}
-}
-
 Controller::Controller(Engine& engine) :
-		Task(engine),
-		zoomFactor(engine.config.get<float>("gameplay.zoom.factor", 0.1f)),
-		panViewXFactor(engine.config.get<float>("gameplay.camera.pan.factor.x", 1.f)),
-		panViewYFactor(engine.config.get<float>("gameplay.camera.pan.factor.y", 1.f)),
-		panViewMouseButtonCode(engine.config.get<unsigned int>("gameplay.camera.pan.mouseButtonCode", 2)) {
+		Task(engine) {
 	engine.events.connect<ApplicationClosedEvent>(*this);
-	engine.events.connect<MouseWheelMoved>(*this);
-	engine.events.connect<MouseButtonPressed>(*this);
-	engine.events.connect<MouseButtonReleased>(*this);
-	engine.events.connect<MouseMoved>(*this);
 
 	engine.tasks.addTask<Renderer>(window);
 	engine.tasks.addTask<SFMLInputProxy>(window);
 	engine.tasks.addTask<InputEcho>();
 	engine.tasks.addTask<VerletIntegrator>();
+	engine.tasks.addTask<CameraMouseController>(window);
 
 	Entity e = engine.entityFactory.createEntity("TestEntity", {});
 	RenderingComponent* renderingComponent = engine.components.getComponent<RenderingComponent>(e);
--- src/tasks/Controller.h
+++ src/tasks/Controller.h
@@ -12,21 +12,7 @@ public:
 	Controller(Engine& engine);
 	void update() override;
 	void receive(ApplicationClosedEvent& closeRequest);
-	void receive(MouseWheelMoved& mouseWheelEvent);
-	void receive(MouseButtonPressed& mouseButtonPressEvent);
-	void receive(MouseButtonReleased& mouseButtonReleaseEvent);
-	void receive(MouseMoved& mouseMoved);
 
 private:
 	sf::RenderWindow window;
-
-	enum {
-		Nothing,
-		PanView
-	} mouseMode{Nothing};
-
-	float zoomFactor;
-	float panViewXFactor;
-	float panViewYFactor;
-	unsigned int panViewMouseButtonCode;
 };
