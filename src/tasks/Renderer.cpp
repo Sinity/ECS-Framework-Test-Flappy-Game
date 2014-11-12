a624a50b2f1f2ae920e1d959116b34faeab1c830
Ezodev <ezo.dev@gmail.com>
2014-11-12 21:12:36 +0100

    Renderer refractorings(mainly auto variables), fixed warnings

--- src/tasks/Renderer.cpp
+++ src/tasks/Renderer.cpp
@@ -7,23 +7,24 @@
 void Renderer::update() {
 	window.clear(fillColor);
 
-	std::vector<PositionComponent*> positions;
-	std::vector<SizeComponent*> sizes;
-	std::vector<GraphicsComponent*> graphics;
+	auto graphics = std::vector<GraphicsComponent*>{};
+	auto sizes = std::vector<SizeComponent*>{};
+	auto positions = std::vector<PositionComponent*>{};
 	engine.components.intersection(graphics, sizes, positions);
 
-	int maxPlane = std::numeric_limits<int>::min();
-	int minPlane = std::numeric_limits<int>::max();
-	for(unsigned int i = 0; i < graphics.size(); i++) {
-		maxPlane = (maxPlane < graphics[i]->plane ? graphics[i]->plane : maxPlane);
-		minPlane = (minPlane > graphics[i]->plane ? graphics[i]->plane : minPlane);
+	auto maxPlane = std::numeric_limits<int>::min();
+	auto minPlane = std::numeric_limits<int>::max();
+	for(auto gfx : graphics) {
+		maxPlane = (maxPlane < gfx->plane ? gfx->plane : maxPlane);
+		minPlane = (minPlane > gfx->plane ? gfx->plane : minPlane);
 	}
 
+    //This stupid slow algorithm is only temporal, until I get some API that have z-ordering natively
 	for(int currentPlane = maxPlane; currentPlane >= minPlane; currentPlane--) {
 		for(size_t i = 0; i < graphics.size(); i++) {
 			if(graphics[i]->plane == currentPlane) {
 				//prepare transform
-				sf::Transform transform;
+				auto transform = sf::Transform{};
 				auto* orientation = engine.components.getComponent<OrientationComponent>(graphics[i]->owner);
 				if(orientation) {
 					transform.rotate(orientation->rotation,
@@ -40,12 +41,13 @@ void Renderer::update() {
 				vertices[3].position = {0, sizes[i]->height};
 
 				vertices[0].texCoords = {0, 0};
-				vertices[1].texCoords = {graphics[i]->texture->getSize().x, 0};
-				vertices[2].texCoords = {graphics[i]->texture->getSize().x, graphics[i]->texture->getSize().y};
-				vertices[3].texCoords = {0, graphics[i]->texture->getSize().y};
+				vertices[1].texCoords = {(float)graphics[i]->texture->getSize().x, 0};
+				vertices[2].texCoords = {(float)graphics[i]->texture->getSize().x, 
+                                         (float)graphics[i]->texture->getSize().y};
+				vertices[3].texCoords = {0, (float)graphics[i]->texture->getSize().y};
 
 				//draw it
-				sf::RenderStates state;
+				auto state = sf::RenderStates{};
 				state.texture = graphics[i]->texture;
 				state.transform = transform;
 				window.draw(vertices, 4, sf::Quads, state);
@@ -59,13 +61,13 @@ void Renderer::update() {
 Renderer::Renderer(Engine& engine, sf::RenderWindow& window) :
 		Task(engine),
 		window(window) {
-	std::string winTitle = engine.config.get("tasks.renderer.windowTitle");
-	unsigned int resX = engine.config.get("tasks.renderer.resolution.x", 1600u);
-	unsigned int resY = engine.config.get("tasks.renderer.resolution.y", 900u);
-	unsigned int clearColorR = engine.config.get<unsigned int>("tasks.renderer.fillColor.red");
-	unsigned int clearColorG = engine.config.get<unsigned int>("tasks.renderer.fillColor.green");
-	unsigned int clearColorB = engine.config.get<unsigned int>("tasks.renderer.fillColor.blue");
-	bool fullscreen = engine.config.get("tasks.renderer.fullscreen", std::string("false")) == "true";
+	auto winTitle = engine.config.get("tasks.renderer.windowTitle");
+	auto resX = engine.config.get("tasks.renderer.resolution.x", 1600u);
+	auto resY = engine.config.get("tasks.renderer.resolution.y", 900u);
+	auto clearColorR = engine.config.get("tasks.renderer.fillColor.red", 0u);
+	auto clearColorG = engine.config.get("tasks.renderer.fillColor.green", 0u);
+	auto clearColorB = engine.config.get("tasks.renderer.fillColor.blue", 0u);
+	auto fullscreen = engine.config.get("tasks.renderer.fullscreen", std::string("false")) == "true";
 
 	if(fullscreen) {
 		window.create(sf::VideoMode::getFullscreenModes()[0], winTitle, sf::Style::Fullscreen);
@@ -75,9 +77,9 @@ Renderer::Renderer(Engine& engine, sf::RenderWindow& window) :
 	}
 	fillColor = sf::Color(clearColorR, clearColorG, clearColorB);
 
-	float left = engine.config.get("tasks.renderer.initialView.left", 0.0f);
-	float top = engine.config.get("tasks.renderer.initialView.top", 0.0f);
-	float width = engine.config.get("tasks.renderer.initialView.width", (float)resX);
-	float height = engine.config.get("tasks.renderer.initialView.height", (float)resY);
+	auto left = engine.config.get("tasks.renderer.initialView.left", 0.0f);
+	auto top = engine.config.get("tasks.renderer.initialView.top", 0.0f);
+	auto width = engine.config.get("tasks.renderer.initialView.width", (float)resX);
+	auto height = engine.config.get("tasks.renderer.initialView.height", (float)resY);
 	window.setView(sf::View({left, top, width, height}));
 }
